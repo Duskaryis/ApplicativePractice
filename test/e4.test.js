de13111bf7aecff8fae735177e77c8e55fc97cd8
Derek <derekpayne1991@hotmail.com>
2024-07-18 14:51:19 -0400

    Solved e3 & e4

--- test/e4.test.js
+++ test/e4.test.js
@@ -3,13 +3,19 @@ import { getPlanetsWithLowGravity } from "../exercises/e4";
 import { data } from "../data/data";
 
 describe("getPlanetsWithLowGravity", () => {
-  it("should exist", () => {
-    expect(getPlanetsWithLowGravity).toBeInstanceOf(Function);
-  });
-  it("There should be 5 planets", () => {
-    expect(getPlanetsWithLowGravity(data).length).toEqual(5);
-  });
-  it("should give me an array of all the planet names", () => {
-    expect(getPlanetsWithLowGravity(data)).toEqual([ 'Uranus', 'Mars', 'Mercure', 'Earth', 'Vénus' ]);
-  });
+	it("should exist", () => {
+		expect(getPlanetsWithLowGravity).toBeInstanceOf(Function);
+	});
+	it("There should be 5 planets", () => {
+		expect(getPlanetsWithLowGravity(data).length).toEqual(5);
+	});
+	it("should give me an array of all the planet names", () => {
+		expect(getPlanetsWithLowGravity(data)).toEqual([
+			"Uranus",
+			"Mars",
+			"Mercure",
+			"Earth",
+			"Vénus",
+		]);
+	});
 });
